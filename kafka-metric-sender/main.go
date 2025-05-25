6fe88ccfdfab7bf7fbf6927be568f3ee1354e1a8
SafarovOrkhan <safarov.on@gmail.com>
2025-05-25 14:11:51 +0400

    update

--- kafka-metric-sender/main.go
+++ kafka-metric-sender/main.go
@@ -1,15 +1,45 @@
 package main
 
 import (
+	"context"
+	"fmt"
+	"log"
+	"math/rand"
 	"time"
+
+	kafkago "github.com/segmentio/kafka-go"
 )
 
 func main() {
-
-	for true {
-
-		// every 40 seconds
-		time.Sleep(40 * time.Second)
+	address := "157.180.115.157:9092"
+	topicName := "mainTopic"
+	min := 10
+	max := 30
+	fmt.Println("Connecting to:", address)
+	// Create connection once
+	conn, err := kafkago.DialLeader(context.Background(), "tcp", address, topicName, 0)
+	if err != nil {
+		log.Fatal("failed to connect to Kafka:", err)
 	}
+	defer conn.Close()
 
+	for {
+		log.Println("Generating value...")
+		value := rand.Intn(max-min) + min
+		message := fmt.Sprintf(`{"cpu": %v}`, value)
+
+		log.Println("Sending message:", message)
+		_, err := conn.WriteMessages(
+			kafkago.Message{
+				Value: []byte(message),
+			},
+		)
+		if err != nil {
+			log.Println("Failed to write message:", err)
+		} else {
+			log.Println("Message sent successfully.")
+		}
+
+		time.Sleep(10 * time.Second)
+	}
 }
